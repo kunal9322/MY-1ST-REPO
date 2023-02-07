b95a69f2f54052b5ae7e5efeac8c3187e851cb01
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-07 23:15:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ API_ID= int(16743442)
 API_HASH= "12bbd720f4097ba7713c5e40a11dfd2a"
 BOT_TOKEN= "6105281766:AAHPNFllRa3kscRfeswDNO85_3NDxWYYp_0"
 
-app= Client(name"kunal", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True) 
+app= Client(name= "kunal", api_id= API_ID, api_hash= API_HASH, bot_token= BOT_TOKEN, in_memory=True) 
 
 #bot filter
 @app.on_message(filters.command(["start", "shuru"]) & filters.private) 
