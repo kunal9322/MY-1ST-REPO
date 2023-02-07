4685e8322358bff93c681b3881d0dcce422030b8
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-07 23:33:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ BOT_TOKEN= "6105281766:AAHPNFllRa3kscRfeswDNO85_3NDxWYYp_0"
 app= Client(name= "kunal", api_id= API_ID, api_hash= API_HASH, bot_token= BOT_TOKEN, in_memory=True) 
 
 #bot filter
-@app.on_message(filters.command(["start", "shuru"]) & filters.private) 
+@app.on_message(filters.command(["start", "shuru","."]) & filters.private) 
 async def _(app,message):
       await message.reply_text("Hello Bro \n I am in now deploying Stage \n After I develop then see my power 😏") 
+
+@app.on_message(filters.command(["help", "."]) & filters.private) 
+async def _(app,message):
+      await message.reply_text("Hello Bro \n My owner @MH17_KUNAL \n thanks to @konnetz") 
+
 
 #starting the bot.. 
 
