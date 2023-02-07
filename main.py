b63b87f3de9e471cfc4f9dae691dca62d81c093f
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-07 22:58:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters, idle
+
+API_ID= int(16743442) 
+API_HASH= "12bbd720f4097ba7713c5e40a11dfd2a"
+BOT_TOKEN= ""
+
+app= Client(name"kunal", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, in_memory=True) 
+
+#bot filter
+@app.on_message(filters.command(["start", "shuru"]) & filters.private) 
+async def _(app.message):
+      await message.reply_text("Hello Bro") 
+
+#starting the bot.. 
+
+app.start() 
+    idle() 
