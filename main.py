b365569a413a272ee5090e8a99ba2f534c879303
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-07 23:17:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app= Client(name= "kunal", api_id= API_ID, api_hash= API_HASH, bot_token= BOT_TO
 
 #bot filter
 @app.on_message(filters.command(["start", "shuru"]) & filters.private) 
-async def _(app.message):
+async def _(app,message):
       await message.reply_text("Hello Bro") 
 
 #starting the bot.. 
