74881382c491a583f6525734afa7480a79d1809a
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-08 06:58:19 +0530

    Update __init__.py

--- Kunal/__init__.py
+++ Kunal/__init__.py
@@ -1,1 +1,7 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, plugins
+
+API_ID= int(16743442) 
+API_HASH= "12bbd720f4097ba7713c5e40a11dfd2a"
+BOT_TOKEN= "6105281766:AAHPNFllRa3kscRfeswDNO85_3NDxWYYp_0"
+
+app= Client(name= "kunal", api_id= API_ID, api_hash= API_HASH, bot_token= BOT_TOKEN, in_memory=True) 
