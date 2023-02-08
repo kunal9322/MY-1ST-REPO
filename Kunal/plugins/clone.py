630eec5501f9eec2db7120e1bb7230609dd7e8e1
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-08 18:44:41 +0530

    Update clone.py

--- Kunal/plugins/clone.py
+++ Kunal/plugins/clone.py
@@ -1,7 +1,7 @@
 from pyrogram import filters, Client
 from Kunal import app, API_ID, API_HASH
 
-@app.on_message(filters.private & filter.command("clone")) 
+@app.on_message(filters.private & filters.command("clone")) 
 async def _(app, message):
     reply = await message.reply("This Command For Clone This Bot\nGo to bot father and send /clone <token>") 
     token = message.command [1]
