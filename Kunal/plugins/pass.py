56f7e954edba6d9da472ca25e3f94f9681973dec
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-08 21:39:19 +0530

    Update pass.py

--- Kunal/plugins/pass.py
+++ Kunal/plugins/pass.py
@@ -1,23 +1,22 @@
-import random, os
+import random
 from pyrogram import Client, filters
 from Kunal import app, API_ID, API_HASH
 
 @app.on_message(filters.private & filters.command("gen"))
-async def _(app, message):    
-    reply = await message.reply('`Processing...`')    
+async def gen_password(app, message):    
+    reply = await message.reply('Processing...')    
     try:
         if len(message.text.split()) > 1:
-            keys, limit = message.text.split()[1], int(message.text.split()[0])
+            limit, keys = int(message.text.split()[0]), message.text.split()[1]
         else:
-            keys = "abcdefghijklmnopqrstuvwxyz"+"1234567890"+"!@#$%^&*()_+".lower()
+            keys = "abcdefghijklmnopqrstuvwxyz" + "1234567890" + "!@#$%^&*()_+".lower()
             limit = int(message.text)
     except:
-        await message.edit_text('Something wrong')
+        await message.edit_text('Something went wrong.')
         return    
     if limit > 100 or limit <= 0:
         text = "Sorry... Failed To Create Password, Because Limit is 1 to 100."
     else:
-        random_value = "".join(random.sample(password, limit))
-        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\ndone"   
+        random_value = "".join(random.sample(keys, limit))
+        text = f"Limit :- {str(limit)}.\nPassword :- {random_value}\n\nDone."   
     await message.edit_text(text, True)
-
