86a2ed63f4c4432589e0989886da21a79d181afc
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-09 08:11:32 +0530

    Create imdb.py

---
+++ Kunal/plugins/imdb.py
@@ -0,0 +1,40 @@
+import requests
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from Kunal import app
+
+
+@app.on_message(filters.command(["imdb", "tmdb"]))
+async def imdb(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("Give me some Movie Name\n\nEx. `/imdb Kgf`")
+    text = (
+        message.text.split(None, 1)[1]
+        if len(message.command) < 3
+        else message.text.split(None, 1)[1].replace(" ", "%20")
+    )
+    url = requests.get(f"https://api.safone.me/tmdb?query={text}").json()["results"][0]
+    await message.reply_photo(
+        photo=url["poster"],
+        caption=f"""**IMDB Movie Details :**
+**Title :** {url["title"]}
+**Description :** {url["overview"]}
+**Rating :** {url["rating"]}
+**Release-Date :** {url["releaseDate"]}
+**Popularity :** {url["popularity"]}
+**Runtime :** {url["runtime"]}
+**Status :** {url["status"]}
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="IMDB Link",
+                        url=url["imdbLink"],
+                    ),
+                ],
+            ],
+        ),
+    )
+
