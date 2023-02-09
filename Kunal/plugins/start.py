a8ffcde99e5754e136d5babafe34dba8d45f039c
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-09 08:31:09 +0530

    Update start.py

--- Kunal/plugins/start.py
+++ Kunal/plugins/start.py
@@ -3,3 +3,15 @@ from pyrogram import filters, Client
 @Client.on_message(filters.command(["start", "shuru","."]) & filters.private) 
 async def _(app:Client, message):
       await message.reply_text("Hello Bro \n I am in now Developing Stage \ntype /clone for clone this bot.. \nfor more information type /help") 
+
+
+reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="IMDB Link",
+                        url=url["imdbLink"],
+                    ),
+                ],
+            ],
+        ),
