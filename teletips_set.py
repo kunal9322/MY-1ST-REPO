8a8999f61189a8db0f1cff9e33bb81374b17c02d
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-17 19:03:14 +0530

    Update teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -1,57 +1,66 @@
-import os
-import logging
 import requests
-from telegram import Update, ChatPermissions
+from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-# Set up logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
+EASYSKY_API_KEY = '26900f01070d5c9fcdd9ece883701597c9b302c1'
+TELEGRAM_BOT_TOKEN = '5815404727:AAGeLb-faQDcZYhkbI32VMof3upWR0YB2bc'
+TELEGRAM_API_ID = 16743442
+TELEGRAM_API_HASH = '12bbd720f4097ba7713c5e40a11dfd2a'
 
-# Get credentials from environment variables
-TELEGRAM_BOT_TOKEN = os.environ.get('5815404727:AAGeLb-faQDcZYhkbI32VMof3upWR0YB2bc')
-TELEGRAM_API_ID = os.environ.get('16743442')
-TELEGRAM_API_HASH = os.environ.get('12bbd720f4097ba7713c5e40a11dfd2a')
-
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hi! I am a bot to help with muting users.\n\nCommands:\n/mute - Mute a user\n/unmute - Unmute a user\n/ban - Ban a user\n/unban - Unban a user')
-
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Commands:\n/mute - Mute a user\n/unmute - Unmute a user\n/ban - Ban a user\n/unban - Unban a user')
-
-def mute(update: Update, context: CallbackContext) -> None:
-    if len(context.args) == 0:
-        update.message.reply_text('Please specify a user to mute.')
-        return
-    user_id = context.args[0]
-    chat_id = update.effective_chat.id
-    try:
-        context.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id, permissions=ChatPermissions())
-        update.message.reply_text(f'User {user_id} has been muted.')
-    except Exception as e:
-        logging.error(e)
-        update.message.reply_text('An error occurred while muting the user. Please try again later.')
-
-def unmute(update: Update, context: CallbackContext) -> None:
-    if len(context.args) == 0:
-        update.message.reply_text('Please specify a user to unmute.')
-        return
-    user_id = context.args[0]
-    chat_id = update.effective_chat.id
-    try:
-        context.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id, permissions=ChatPermissions(can_send_messages=True))
-        update.message.reply_text(f'User {user_id} has been unmuted.')
-    except Exception as e:
-        logging.error(e)
-        update.message.reply_text('An error occurred while unmuting the user. Please try again later.')
+# Define ban, unban, mute, and unmute functions
 
 def ban(update: Update, context: CallbackContext) -> None:
-    if len(context.args) == 0:
-        update.message.reply_text
+    user_id = update.message.reply_to_message.from_user.id
+    context.bot.kick_chat_member(update.message.chat_id, user_id)
+    update.message.reply_text(f"{user_id} has been banned from the chat.")
 
-        updater.start_polling()
-        updater.idle()
+def unban(update: Update, context: CallbackContext) -> None:
+    user_id = context.args[0]
+    context.bot.unban_chat_member(update.message.chat_id, user_id)
+    update.message.reply_text(f"{user_id} has been unbanned from the chat.")
 
+def mute(update: Update, context: CallbackContext) -> None:
+    user_id = update.message.reply_to_message.from_user.id
+    context.bot.restrict_chat_member(update.message.chat_id, user_id, can_send_messages=False)
+    update.message.reply_text(f"{user_id} has been muted in the chat.")
 
+def unmute(update: Update, context: CallbackContext) -> None:
+    user_id = context.args[0]
+    context.bot.restrict_chat_member(update.message.chat_id, user_id, can_send_messages=True)
+    update.message.reply_text(f"{user_id} has been unmuted in the chat.")
+
+# Define existing functions
+
+def shorten_url(long_url):
+    url = f'https://easysky.in/api/shorten?url={long_url}&apikey={EASYSKY_API_KEY}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()['shortenedUrl']
+    else:
+        return None
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hi! Send me a URL and I will shorten it for you.')
+
+def shorten(update: Update, context: CallbackContext) -> None:
+    long_url = update.message.text
+    shortened_url = shorten_url(long_url)
+    if shortened_url is not None:
+        update.message.reply_text(shortened_url)
+    else:
+        update.message.reply_text('Sorry, an error occurred while shortening the URL.')
+
+def main() -> None:
+    updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("shorten", shorten))
+    dispatcher.add_handler(CommandHandler("ban", ban))
+    dispatcher.add_handler(CommandHandler("unban", unban))
+    dispatcher.add_handler(CommandHandler("mute", mute))
+    dispatcher.add_handler(CommandHandler("unmute", unmute))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
