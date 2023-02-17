3749dd7cde3625d96523a9a5ed856f4e0af566b2
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-17 18:16:34 +0530

    Update teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -7,8 +7,10 @@ TELEGRAM_BOT_TOKEN = '5815404727:AAGeLb-faQDcZYhkbI32VMof3upWR0YB2bc'
 TELEGRAM_API_ID = 16743442
 TELEGRAM_API_HASH = '12bbd720f4097ba7713c5e40a11dfd2a'
 
-def shorten_url(long_url):
+def shorten_url(long_url, alias=None):
     url = f'https://easysky.in/api/shorten?url={long_url}&apikey={EASYSKY_API_KEY}'
+    if alias:
+        url += f'&custom={alias}'
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()['shortenedUrl']
@@ -22,12 +24,17 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Commands:\n/start - Start the bot\n/shorten - Shorten a URL\n/stats - Get statistics\n/about - About the bot')
 
 def shorten(update: Update, context: CallbackContext) -> None:
-    long_url = update.message.text
-    shortened_url = shorten_url(long_url)
-    if shortened_url is not None:
-        update.message.reply_text(shortened_url)
+    args = context.args
+    if len(args) == 0:
+        update.message.reply_text('Please provide a URL to shorten.')
     else:
-        update.message.reply_text('Sorry, an error occurred while shortening the URL.')
+        long_url = args[0]
+        alias = args[1] if len(args) > 1 else None
+        shortened_url = shorten_url(long_url, alias)
+        if shortened_url is not None:
+            update.message.reply_text(shortened_url)
+        else:
+            update.message.reply_text('Sorry, an error occurred while shortening the URL.')
 
 def stats(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Sorry, statistics are not available yet.')
@@ -45,6 +52,6 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("about", about))
     updater.start_polling()
     updater.idle()
-print('Bot Started Successfully') 
+
 if __name__ == '__main__':
     main()
