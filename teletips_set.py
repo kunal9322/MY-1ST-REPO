e022126e76d6a6aa354129335b24ec3785e1ec08
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-17 18:35:53 +0530

    Update teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -1,68 +1,52 @@
+import os
+import logging
 import requests
 from telegram import Update, ChatPermissions
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-TELEGRAM_BOT_TOKEN = '5815404727:AAGeLb-faQDcZYhkbI32VMof3upWR0YB2bc'
-TELEGRAM_API_ID = 16743442
-TELEGRAM_API_HASH = '12bbd720f4097ba7713c5e40a11dfd2a'
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+# Get credentials from environment variables
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+TELEGRAM_API_ID = os.environ.get('TELEGRAM_API_ID')
+TELEGRAM_API_HASH = os.environ.get('TELEGRAM_API_HASH')
 
 def start(update: Update, context: CallbackContext) -> None:
-update.message.reply_text('Hi! I am a bot to help with muting users.\n\nCommands:\n/mute - Mute a user\n/unmute - Unmute a user\n/ban - Ban a user\n/unban - Unban a user')
+    update.message.reply_text('Hi! I am a bot to help with muting users.\n\nCommands:\n/mute - Mute a user\n/unmute - Unmute a user\n/ban - Ban a user\n/unban - Unban a user')
 
 def help_command(update: Update, context: CallbackContext) -> None:
-update.message.reply_text('Commands:\n/mute - Mute a user\n/unmute - Unmute a user\n/ban - Ban a user\n/unban - Unban a user')
+    update.message.reply_text('Commands:\n/mute - Mute a user\n/unmute - Unmute a user\n/ban - Ban a user\n/unban - Unban a user')
 
 def mute(update: Update, context: CallbackContext) -> None:
-if len(context.args) == 0:
-update.message.reply_text('Please specify a user to mute.')
-return
-user_id = context.args[0]
-chat_id = update.effective_chat.id
-try:
-context.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id, permissions=ChatPermissions())
-update.message.reply_text(f'User {user_id} has been muted.')
-except Exception as e:
-update.message.reply_text(f'Error: {e}')
+    if len(context.args) == 0:
+        update.message.reply_text('Please specify a user to mute.')
+        return
+    user_id = context.args[0]
+    chat_id = update.effective_chat.id
+    try:
+        context.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id, permissions=ChatPermissions())
+        update.message.reply_text(f'User {user_id} has been muted.')
+    except Exception as e:
+        logging.error(e)
+        update.message.reply_text('An error occurred while muting the user. Please try again later.')
 
 def unmute(update: Update, context: CallbackContext) -> None:
-if len(context.args) == 0:
-update.message.reply_text('Please specify a user to unmute.')
-return
-user_id = context.args[0]
-chat_id = update.effective_chat.id
-try:
-context.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id, permissions=ChatPermissions(can_send_messages=True))
-update.message.reply_text(f'User {user_id} has been unmuted.')
-except Exception as e:
-update.message.reply_text(f'Error: {e}')
+    if len(context.args) == 0:
+        update.message.reply_text('Please specify a user to unmute.')
+        return
+    user_id = context.args[0]
+    chat_id = update.effective_chat.id
+    try:
+        context.bot.restrict_chat_member(chat_id=chat_id, user_id=user_id, permissions=ChatPermissions(can_send_messages=True))
+        update.message.reply_text(f'User {user_id} has been unmuted.')
+    except Exception as e:
+        logging.error(e)
+        update.message.reply_text('An error occurred while unmuting the user. Please try again later.')
 
 def ban(update: Update, context: CallbackContext) -> None:
-if len(context.args) == 0:
-update.message.reply_text('Please specify a user to ban.')
-return
-user_id = context.args[0]
-chat_id = update.effective_chat.id
-try:
-context.bot.kick_chat_member(chat_id=chat_id, user_id=user_id)
-update.message.reply_text(f'User {user_id} has been banned.')
-except Exception as e:
-update.message.reply_text(f'Error: {e}')
-
-def unban(update: Update, context: CallbackContext) -> None:
-if len(context.args) == 0:
-update.message.reply_text('Please specify a user to unban.')
-return
-user_id = context.args[0]
-chat_id = update.effective_chat.id
-try:
-context.bot.unban_chat_member(chat_id=chat_id, user_id=user_id)
-update.message.reply_text(f'User {user_id} has been unbanned.')
-except Exception as e:
-update.message.reply_text(f'Error: {e}')
-
-def main() -> None:
-updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(CommandHandler("help", help_command))
-dispatcher.add_handler(CommandHandler("mute", mute))
+    if len(context.args) == 0:
+        update.message.reply_text
