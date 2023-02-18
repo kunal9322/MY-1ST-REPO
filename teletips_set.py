b60d082e312ac64070556aa22a832dd8249b0adb
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-18 08:57:11 +0530

    Update teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -1,68 +1,37 @@
-import telegram
-import requests
-from telegram.ext import CommandHandler, Updater
+from pyrogram import Client, filters
+import random
 
-# Set the Telegram API token and the easysky.in API credentials
-TOKEN = '6105281766:AAHPNFllRa3kscRfeswDNO85_3NDxWYYp_0'
-API_ID = '16743442'
-API_HASH = '12bbd720f4097ba7713c5e40a11dfd2a'
-API_ENDPOINT = 'https://easysky.in/api?api=26900f01070d5c9fcdd9ece883701597c9b302c1'
+# Create a new Pyrogram client instance
+app = Client("6105281766:AAHPNFllRa3kscRfeswDNO85_3NDxWYYp_0")
 
-# Create the bot and add the command handlers
-bot = telegram.Bot(token=TOKEN)
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+# Define a handler for the /start command
+@app.on_message(filters.command("start"))
+def start_command_handler(client, message):
+    response = "Welcome to my game bot! To play, type /play."
+    client.send_message(message.chat.id, response)
 
-# Define the /start command handler
-def start_handler(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the URL shortener bot! Use /help to see the available commands.")
+# Define a handler for the /play command
+@app.on_message(filters.command("play"))
+def play_command_handler(client, message):
+    number = random.randint(1, 10)
+    response = f"Guess a number between 1 and 10!"
+    client.send_message(message.chat.id, response)
 
-# Define the /help command handler
-def help_handler(update, context):
-    help_text = "Available commands:\n\n"
-    help_text += "/start - Start the bot.\n"
-    help_text += "/help - Show the available commands.\n"
-    help_text += "/ping - Check if the bot is online.\n"
-    help_text += "/shorten <url> - Shorten a URL using easysky.in."
-    context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
+    # Define a handler for incoming guesses
+    @app.on_message(filters.text)
+    def guess_handler(client, message):
+        try:
+            guess = int(message.text)
+            if guess == number:
+                response = f"Congratulations, {message.from_user.first_name}! You guessed the number!"
+            elif guess < number:
+                response = "Too low, try again."
+            else:
+                response = "Too high, try again."
+        except ValueError:
+            response = "Invalid guess, please enter a number."
+        
+        client.send_message(message.chat.id, response)
 
-# Define the /ping command handler
-def ping_handler(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Pong!")
-
-# Define the /shorten command handler
-def shorten_handler(update, context):
-    # Extract the URL from the command arguments
-    args = context.args
-    if len(args) == 0:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a URL to shorten.")
-        return
-    url = args[0]
-
-    # Call the easysky.in API to shorten the URL
-    params = {'api_id': API_ID, 'api_hash': API_HASH, 'url': url}
-    response = requests.post(API_ENDPOINT, data=params)
-    if response.status_code != 200:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Error shortening the URL.")
-        return
-    short_url = response.text
-
-    # Send the shortened URL to the user
-    context.bot.send_message(chat_id=update.effective_chat.id, text=short_url)
-
-# Add the command handlers to the dispatcher
-start_handler = CommandHandler('start', start_handler)
-dispatcher.add_handler(start_handler)
-
-help_handler = CommandHandler('help', help_handler)
-dispatcher.add_handler(help_handler)
-
-ping_handler = CommandHandler('ping', ping_handler)
-dispatcher.add_handler(ping_handler)
-
-shorten_handler = CommandHandler('shorten', shorten_handler)
-dispatcher.add_handler(shorten_handler)
-
-# Start the bot
-updater.start_polling()
-print('bot running smooth') 
+# Start the client
+app.run()
