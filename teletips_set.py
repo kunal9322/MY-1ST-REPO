a78fba3a1f8a35725ed9b65812cc953acaa240db
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-02-18 09:07:00 +0530

    Update teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -1,12 +1,11 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, idle
 import random
 
 # Create a new Pyrogram client instance
-# Create a new Pyrogram client instance
 app = Client(
-    "6105281766:AAHPNFllRa3kscRfeswDNO85_3NDxWYYp_0",
-    api_id=16743442,  # replace with your API ID
-    api_hash="12bbd720f4097ba7713c5e40a11dfd2a"  # replace with your API hash
+    "my_bot_token",
+    api_id=12345,  # replace with your API ID
+    api_hash="0123456789abcdef0123456789abcdef"  # replace with your API hash
 )
 
 # Define a handler for the /start command
@@ -22,21 +21,22 @@ def play_command_handler(client, message):
     response = f"Guess a number between 1 and 10!"
     client.send_message(message.chat.id, response)
 
-    # Define a handler for incoming guesses
-    @app.on_message(filters.text)
-    def guess_handler(client, message):
-        try:
-            guess = int(message.text)
-            if guess == number:
-                response = f"Congratulations, {message.from_user.first_name}! You guessed the number!"
-            elif guess < number:
-                response = "Too low, try again."
-            else:
-                response = "Too high, try again."
-        except ValueError:
-            response = "Invalid guess, please enter a number."
-        
-        client.send_message(message.chat.id, response)
+# Define a handler for incoming guesses
+@app.on_message(filters.text)
+def guess_handler(client, message):
+    try:
+        guess = int(message.text)
+        if guess == number:
+            response = f"Congratulations, {message.from_user.first_name}! You guessed the number!"
+        elif guess < number:
+            response = "Too low, try again."
+        else:
+            response = "Too high, try again."
+    except ValueError:
+        response = "Invalid guess, please enter a number."
+    
+    client.send_message(message.chat.id, response)
 
 # Start the client
 app.run()
+idle() 
